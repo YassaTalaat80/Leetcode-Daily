5f55aac3a4fce8c76c501ac5edec17b924f9b964
Yassa Talaat <145781920+YassaTalaat80@users.noreply.github.com>
2026-06-02 18:48:07 +0300

    Time: 5 ms (96.09%), Memory: 44.58 MB (51.02%) - LeetCode

---
+++ 2026/JUN/01-Minimum Cost of Buying Candies With Discount.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+  public int minimumCost(int[] cost) {
+    int n = cost.length;
+    Arrays.sort(cost);
+    int ans = 0;
+    if (n <= 2) {
+      for (int i = 0; i < n; i++) {
+        ans += cost[i];
+      }
+    } else {
+      for (int i = n - 1; i >= 0; ) {
+        if (i - 2 >= 0) {
+          ans += cost[i - 1] + cost[i];
+        } else {
+          for (int j = i; j >= 0; j--) {
+            ans += cost[j];
+          }
+        }
+        i -= 3;
+      }
+    }
+    return ans;
+  }
+}
